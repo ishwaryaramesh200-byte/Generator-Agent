97515641a7a8dbb0be05abb4534c89cb7ed328b8
Ishwarya Ramesh <ishwaryaramesh200@gmail.com>
2026-02-21 15:13:02 +0530

    Agent Name Changed

--- main.js
+++ main.js
@@ -3,19 +3,14 @@ const vscode = require('vscode');
 // Agent objects that will be discovered by Copilot
 const agents = [
     {
-        id: 'conditional-generator',
-        name: 'Conditional Generator',
-        description: 'Generates text based on conditional rules',
-    },
-    {
         id: 'generators',
         name: 'Generators',
-        description: 'Generator Configuration Agent responsible for generating generators',
+        description: 'Generator Agent responsible for generating generators',
     }
 ];
 
 function activate(context) {
-    console.log('Conditional Generator Agent is now active!');
+    console.log('Generator Agent is now active!');
 
     // Export agents for Copilot discovery
     if (vscode.window && vscode.window.registerChatAgents) {
@@ -31,9 +26,9 @@ function activate(context) {
 
     // Command to trigger the agent
     let disposable = vscode.commands.registerCommand(
-        'conditional-generator.start',
+        'generator.start',
         function () {
-            vscode.window.showInformationMessage('Conditional Generator Agent started!');
+            vscode.window.showInformationMessage('Generator Agent started!');
         }
     );
 
